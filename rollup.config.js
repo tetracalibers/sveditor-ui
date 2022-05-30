2a4ad595f15a9d197d4537cab63e0849962098e6
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-05-30 14:35:17 +0900

    パッケージ初期化

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import svelte from 'rollup-plugin-svelte'
+import resolve from 'rollup-plugin-node-resolve'
+
+const pkg = require('./package.json')
+
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, (m) => m.toUpperCase())
+	.replace(/-\w/g, (m) => m[1].toUpperCase())
+
+export default {
+	input: 'src/index.js',
+	output: [
+		{ file: pkg.module, format: 'en' },
+		{ file: pkg.main, format: 'umd', name: name },
+	],
+	plugins: [svelte(), resolve()],
+}
