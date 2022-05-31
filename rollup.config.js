5ef82edcc3428095e75448018257644c4d2f02e0
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-05-31 10:55:09 +0900

    Pug、Stylus、TypeScriptのセットアップ

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import svelte from 'rollup-plugin-svelte'
 import resolve from 'rollup-plugin-node-resolve'
+import sveltePreprocess from 'svelte-preprocess'
+import typescript from '@rollup/plugin-typescript'
 
 const pkg = require('./package.json')
 
@@ -8,11 +10,31 @@ const name = pkg.name
 	.replace(/^\w/, (m) => m.toUpperCase())
 	.replace(/-\w/g, (m) => m[1].toUpperCase())
 
+const production = !process.env.ROLLUP_WATCH
+
 export default {
 	input: 'src/index.js',
 	output: [
 		{ file: pkg.module, format: 'en' },
 		{ file: pkg.main, format: 'umd', name: name },
 	],
-	plugins: [svelte(), resolve()],
+	plugins: [
+		typescript({ sourceMap: !production }),
+		svelte({
+			preprocess: sveltePreprocess({
+				sourceMap: !production,
+				postcss: {
+					plugins: [require('autoprefixer')()],
+				},
+				stylus: {
+					paths: ['src/stylus'],
+				},
+			}),
+			dev: !production,
+			css: (css) => {
+				css.write('dist/bundle.css')
+			},
+		}),
+		resolve(),
+	],
 }
